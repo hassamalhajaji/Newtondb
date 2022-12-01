fc27e4781ddac7a7715244947cdae276019c2086
Tommy Seo <tommyseo7m@gmail.com>
2022-12-01 09:01:25 -0800

    Added file that contains dicts of global vars

---
+++ containers.py
@@ -0,0 +1,40 @@
+#every variable besides derivative
+mechanicsVars = {
+	#lowercase letters
+	"v" : "velocity",
+	"a" : "acceleration",
+	"h" : "plancks constant",
+	"m" : "mass",
+	"g" : "gravity",
+	"p" : "momentum",
+	"v" : "velocity",
+	"r" : "radius",
+	#uppercase letters
+	"ff" : "force",
+	"ww" : "work",
+	"jj": "joule",
+	"pp" : "power",
+	"ii" : "inertia",
+	"ll" : "angular momentum",
+	"hh" : "angular impulse",
+	"gg" : "gravitational constant",
+	"tt" : "period of pendulum",
+	#greek letters (U for uppercase, L for lowercase)
+	"alphaL" : "angular acceleration",
+	"tauL" : "torque",
+	"thetaU" : "angular position",
+	"deltaU" : "difference",
+	"etaU": "angular velocity",
+	"omegaL" : "angular frequency",
+	#subscripts
+	"fs" : "static friction",
+	"fg" : "universal gravitation",
+	"vg" : "gravitational potential",
+	"fk" : "kinetic friction",
+	"ac" : "centripetal acceleration",
+	"uug" : "gravitational potential energy",
+	"ein" : "energy in",
+	"wwout" : "work out",
+}
+
+mechanicsEqs = {}
