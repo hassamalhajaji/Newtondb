b9b11e12eea40e62ffa79a34e6f6675eb56a5c54
Tommy Seo <tommyseo7m@gmail.com>
2022-12-14 17:04:30 -0800

    added more variables

---
+++ containers.py
@@ -0,0 +1,59 @@
+#every variable besides derivative
+mechanicsVars = {
+	#lowercase letters
+	"v" : "velocity",
+	"a" : "acceleration",
+	"h" : "plancks constant",
+	"m" : "mass",
+	"g" : "gravity",
+	"p" : "momentum",
+	"v" : "velocity",
+	"r" : "radius",
+	"k" : "spring constant",
+	"x" : "amount of extension",
+	#uppercase letters
+	"aa" : "area",
+	"bb" : "elastic property",
+	"ff" : "force",
+	"ww" : "work",
+	"jj": "joule",
+	"pp" : "power",
+	"ii" : "inertia",
+	"ll" : "angular momentum",
+	"hh" : "angular impulse",
+	"gg" : "gravitational constant",
+	"tt" : "period of pendulum",
+	"ee" : "youngs modulus",
+	"vv" : "volume",
+	#greek letters (U for uppercase, L for lowercase)
+	"alphaL" : "angular acceleration",
+	"tauL" : "torque",
+	"thetaU" : "angular position",
+	"deltaU" : "difference",
+	"etaU": "angular velocity",
+	"omegaL" : ["angular frequency", "angular velocity"],
+	"sigmaL" : "strain; uniaxial force per unit surface",
+	"epsilonL": "stress; proportional deformation",
+	"rho" : "inertial property",
+	#subscripts
+	"fs" : "static friction",
+	"ll" : "angular momentum",
+	"ii" : "moment of intertia",
+	"fg" : "universal gravitation",
+	"vg" : "gravitational potential",
+	"fk" : "kinetic friction",
+	"ac" : "centripetal acceleration",
+	"uug" : "gravitational potential energy",
+	"ein" : "energy in",
+	"wwout" : "work out",
+}
+
+
+#simple harmonic motion acceleration
+#viscous flow
+#continuity equation
+#Instantaneous Acceleration
+#
+
+
+mechanicsEqs = {}
